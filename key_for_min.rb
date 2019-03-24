a0e6df19878a5ba0bde33728e46881f3f09eafcc
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-03-24 17:28:09 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,37 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+#
+# ikea = {:chair => 25, :table => 85, :mattress => 450}
+# key_for_min_value(ikea)
+# # => :chair
+#
+# veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+# key_for_min_value(veggies)
+# # => "apple"
 
 def key_for_min_value(name_hash)
+if name_hash.empty?  == true
+  nil
+else
+  smallest_value = 0
+  smallestkey = ""
+  name_hash.collect do|key, value|
+    smallest_value = value
+    puts "#{smallest_value} is an array value"
+  end
 
+  name_hash.collect do |key, value|
+    if value < smallest_value
+    smallest_value = value
+  end
+end
+
+  name_hash.each do |key, value|
+    if value == smallest_value
+      smallestkey = key
+    end
+  end
+
+smallestkey
+end
 end
